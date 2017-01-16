a696714d361a562a68e57ebc0b25a18f1e1e62ad
may-k <mayk.4b@gmail.com>
2017-01-16 18:33:26 +0200

    version 0

---
+++ js/main.js
@@ -0,0 +1,57 @@
+/*jslint white:true */
+/*global angular */
+var app = angular.module('myApp', ["ngRoute", "ngAnimate"]);
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "partial/home.html"
+    })
+     .when("/samsung", {
+        templateUrl : "partial/samsung.html"
+    })
+      .when("/apple", {
+        templateUrl : "partial/apple.html"
+    })
+      .when("/lenovo", {
+        templateUrl : "partial/lenovo.html"
+    })
+      .when("/huawei", {
+        templateUrl : "partial/huawei.html"
+    })
+      .when("/htc", {
+        templateUrl : "partial/htc.html"
+    })
+      .when("/lg", {
+        templateUrl : "partial/lg.html"
+    })
+      .when("/sony", {
+        templateUrl : "partial/sony.html"
+    })
+      .when("/allphones", {
+        templateUrl : "partial/allphones.html"
+    })
+    .when("/search", {
+        templateUrl : "partial/search.html" ,
+         controller :"customersCtrl"
+    })
+    
+    .when("/specs/:id", {
+        templateUrl :"partial/specs.html",
+        controller :"customersCtrl"
+    });
+    $locationProvider.html5Mode(true);
+});
+
+app.controller('customersCtrl',  function ($scope, $http, $routeParams, $location , $rootScope) {
+ 
+    $http.get("2ndproject.json")
+        .then(function (response) {$scope.phones = response.data.phones;
+                                  $scope.id = $routeParams.id; });
+ $scope.result = function(Q) {$location.path("/search");
+                              $rootScope.Q = Q ;
+                             }
+ $scope.enter =function(sth) { 
+     if(sth.keyCode === 13) { $scope.result}
+ }
+});
+      
