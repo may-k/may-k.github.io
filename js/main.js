644764d375855e9b23931fb02dbde6d916021f7d
may-k <mayk.4b@gmail.com>
2017-02-03 19:10:14 +0200

    v0.8

--- js/main.js
+++ js/main.js
@@ -55,39 +55,21 @@ app.controller('customersCtrl',  function ($scope, $http, $routeParams, $locatio
  }
 });
 
-$(document).ready(function() {
-        
-   /* activate the carousel */
-   $("#modal-carousel").carousel({interval:false});
 
-   /* change modal title when slide changes */
-   $("#modal-carousel").on("slid.bs.carousel",       function () {
-        $(".modal-title")
-        .html($(this)
-        .find(".active img")
-        .attr("title"));
-   });
-
-   /* when clicking a thumbnail */
-   $(".row .thumbnail").click(function(){
-    var content = $(".carousel-inner");
-    var title = $(".modal-title");
-  
-    content.empty();  
-    title.empty();
-  
-  	var id = this.id;  
-     var repo = $("#img-repo .item");
-     var repoCopy = repo.filter("#" + id).clone();
-     var active = repoCopy.first();
-  
-    active.addClass("active");
-    title.html(active.find("img").attr("title"));
-  	content.append(repoCopy);
-
-    // show the modal
-  	$("#modal-gallery").modal("show");
-  });
-
+$(function(){
+	//picturesEyes($('li'));
+	$('li').picEyes();
 });
-      
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
