85dde456723acc683d9488d2c0163cbf1f96012d
may-k <mayk.4b@gmail.com>
2017-02-11 14:51:26 +0200

    1.0.4

--- js/main.js
+++ js/main.js
@@ -109,91 +109,40 @@ function myfg(ev){
        };
     
 /*slider*/
-    $(function() {
-				$('#slider').carouFredSel({
-					width: '100%',
-					align: false,
-					items: 3,
-					items: {
-						width: $('#wrapper').width() * 0.15,
-						height: 500,
-						visible: 1,
-						minimum: 1
-					},
-					scroll: {
-						items: 1,
-						timeoutDuration : 5000,
-						onBefore: function(data) {
-			
-							//	find current and next slide
-							var currentSlide = $('.slide.active', this),
-								nextSlide = data.items.visible,
-								_width = $('#wrapper').width();
-			
-							//	resize currentslide to small version
-							currentSlide.stop().animate({
-								width: _width * 0.15
-							});		
-							currentSlide.removeClass( 'active' );
-			
-							//	hide current block
-							data.items.old.add( data.items.visible ).find( '.slide-block' ).stop().fadeOut();					
-			
-							//	animate clicked slide to large size
-							nextSlide.addClass( 'active' );
-							nextSlide.stop().animate({
-								width: _width * 0.7
-							});						
-						},
-						onAfter: function(data) {
-							//	show active slide block
-							data.items.visible.last().find( '.slide-block' ).stop().fadeIn();
-						}
-					},
-					onCreate: function(data){
-			
-						//	clone images for better sliding and insert them dynamacly in slider
-						var newitems = $('.slide',this).clone( true ),
-							_width = $('#wrapper').width();
+    
+        jssor_1_slider_init = function() {
 
-						$(this).trigger( 'insertItem', [newitems, newitems.length, false] );
-			
-						//	show images 
-						$('.slide', this).fadeIn();
-						$('.slide:first-child', this).addClass( 'active' );
-						$('.slide', this).width( _width * 0.15 );
-			
-						//	enlarge first slide
-						$('.slide:first-child', this).animate({
-							width: _width * 0.7
-						});
-			
-						//	show first title block and hide the rest
-						$(this).find( '.slide-block' ).hide();
-						$(this).find( '.slide.active .slide-block' ).stop().fadeIn();
-					}
-				});
-			
-				//	Handle click events
-				$('#slider').children().click(function() {
-					$('#slider').trigger( 'slideTo', [this] );
-				});
-			
-				//	Enable code below if you want to support browser resizing
-				$(window).resize(function(){
-			
-					var slider = $('#slider'),
-						_width = $('#wrapper').width();
-			
-					//	show images
-					slider.find( '.slide' ).width( _width * 0.15 );
-			
-					//	enlarge first slide
-					slider.find( '.slide.active' ).width( _width * 0.7 );
-			
-					//	update item width config
-					slider.trigger( 'configuration', ['items.width', _width * 0.15] );
-				});
+            var jssor_1_options = {
+              $AutoPlay: true,
+              $SlideDuration: 800,
+              $SlideEasing: $Jease$.$OutQuint,
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              }
+            };
 
-			});
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            /*responsive code begin*/
+            /*you can remove responsive code if you don't want the slider scales while window resizing*/
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 1920);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $Jssor$.$AddEvent(window, "load", ScaleSlider);
+            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+            /*responsive code end*/
+        };
+    /*slider ends*/
 });
